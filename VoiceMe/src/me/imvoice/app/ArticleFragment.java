ce29d8fe689e163b4046abb2a0552a98f1db2111
aoisky <yangdon8942@gmail.com>
2013-11-02 10:33:44 -0400

    some changes in the default aricle list

--- VoiceMe/src/me/imvoice/app/ArticleFragment.java
+++ VoiceMe/src/me/imvoice/app/ArticleFragment.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-
+/**
+ * Show a list of articles
+ * @author Yudong Yang
+ *
+ */
 public class ArticleFragment extends ListFragment{
 
 	@Override
@@ -21,10 +25,9 @@ public class ArticleFragment extends ListFragment{
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
 
-		String sideBarList[] = {"Article1","Article2","Article3","Article4","More Articles..."};
+		String sideBarList[] = {"Purdue University development, investment offices ","Article2","Article3","Article4","Drug may reduce chronic pain for spinal cord injuries","More Articles..."};
 		ArrayAdapter<String> articleAdapter = new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_list_item_1, sideBarList);
 		setListAdapter(articleAdapter);
 	    
-
 	}
 }
