59ade84e972b745abdc10e62ee179246775dcea5
michaelize <michaelmicwang@gmail.com>
2013-11-07 19:14:41 -0500

    Added post content comment and fetch content comment APIs

--- VoiceMe/server/functions.php
+++ VoiceMe/server/functions.php
@@ -64,6 +64,116 @@
         
         echo json_encode($results);
     }
+    /**
+        Incoming arguments: $body['uid'], $body['comment'], $body['rid'],$body['pid']
+        Return value(key:variable):     result:$success
+    */
+    else if ($_SERVER['HTTP_METHOD'] === 'post_comment'){
+    
+        $stmt = $db->prepare('INSERT INTO  `comment` (`cid`, `pid`, `rid` ,`comment` ,`uid`,`reply_count`,
+                                            `comment_count`, `creation_time`, `last_update_time`)
+                                             VALUES (NULL , ?, ?, ?, 0, 0, now(), NULL) ');
+        $stmt->bind_param('sss', $body['rid'], $body['pid'], $body['comment'],$body['uid']);
+        if($stmt->execute()){
+            $stmt = $db->prepare('SELECT `cid`,`comment`,`rid` FROM  `comment` WHERE `uid` = ? ORDER BY `creation_time` LIMIT 1');
+            $stmt->bind_param('s', $body['uid']);
+            $stmt->execute();
+            $stmt -> bind_result($cid,$post_comment,$rid);
+            $stmt->fetch();
+            if($rid != NULL){
+                $stmt = $db->prepare('SELECT `uid` FROM  `comment` WHERE `cid` = ? LIMIT 1');
+                $stmt->bind_param('s', $cid);
+                $stmt->execute();
+                $stmt -> bind_result($r_uid);
+                $stmt->fetch();
+            }
+            if($body['comment'] === $post_comment){
+                $stmt = $db->prepare('INSERT INTO  `user_comment_list` (`uid`, `r_uid`, `rid`, `cid`)
+                                             VALUES (?, ?, ?, ?) ');
+                $stmt->bind_param('ss', $body['uid'], $r_uid,$body['rid'],$cid);
+                if($stmt->execute()){
+                    $success = "Success";
+                }
+                else{
+                    $success = "Failed";
+                }
+            }
+        }
+        else{
+            $success = "Failed";
+        }
+        $results[] = array('result'=>$success);
+        $stmt -> close();
+        $db->close();
+        
+        echo json_encode($results);
+    }
+    /**
+        Incoming arguments: $body['uid'], $body['pid']
+        Return value(key:variable):     result:$success post_content:$post_content share_count:$share_count like_count:$like_count comment_count:$comment_count creation_time:$creation_time
+    */
+    else if ($_SERVER['HTTP_METHOD'] === 'fetch_content'){
+    
+        //$stmt = $db->prepare('INSERT INTO  `content` (`pid` ,`post_content` ,`uid`,`share_count`,
+                                            //`like_count`, `comment_count`, `creation_time`, `last_update_time`)
+                                            // VALUES (NULL , ?, ?, 0, 0, 0, now(), NULL) ');
+        //$stmt->bind_param('ss', $body['post_content'],$body['uid']);
+        //if($stmt->execute()){
+        $stmt = $db->prepare('SELECT `post_content`,`share_count`,`like_count`,`comment_count`,`creation_time` FROM  `content` WHERE `pid` = ? AND `uid`= ? LIMIT 1');
+        $stmt->bind_param('ss', $body['pid'], $body['uid']);
+        $stmt->execute();
+        $stmt -> bind_result($post_content,$share_count,$like_count,$comment_count,$creation_time);
+        $stmt->fetch();
+        if($post_content !== NULL){
+            $success = "Success";
+        }
+        //}
+        else{
+            $success = "Failed";
+        }
+        $results[] = array('result'=>$success,'post_content'=>$post_content,'share_count'=>$share_count,'like_count'=>$like_count,'comment_count'=>$comment_count,'creation_time'=>$creation_time);
+        $stmt -> close();
+        $db->close();
+        
+        echo json_encode($results);
+    }
+    /**
+        Incoming arguments: $body['uid'], $body['cid'], $body['pid']
+        Return value(key:variable):     result:$success comment:$comment rid:$rid reply_count:reply_count comment_count:comment_count creation_time:creation_time
+    */
+    else if ($_SERVER['HTTP_METHOD'] === 'fetch_comment'){
+    
+        //$stmt = $db->prepare('INSERT INTO  `comment` (`cid`, `rid` ,`comment` ,`uid`,`reply_count`,
+                                            //`comment_count`, `creation_time`, `last_update_time`)
+                                            // VALUES (NULL , ?, ?, ?, 0, 0, now(), NULL) ');
+        //$stmt->bind_param('sss', $body['rid'], $body['comment'],$body['uid']);
+        //if($stmt->execute()){
+            $stmt = $db->prepare('SELECT `cid`,`pid`, `comment`,`rid`,`reply_count`,`comment_count`,`creation_time` FROM  `comment` WHERE `cid` = ? AND `uid`= ? ORDER BY `creation_time` LIMIT 1');
+            $stmt->bind_param('ss', $body['cid'],$body['uid']);
+            $stmt->execute();
+            $stmt -> bind_result($cid,$post_comment,$rid,$reply_count,$comment_count,$creation_time);
+            $stmt->fetch();
+            if($cid !== NULL){
+                $stmt = $db->prepare('SELECT `uid` FROM  `comment` WHERE `cid` = ? LIMIT 1');
+                $stmt->bind_param('ss', $cid);
+                $stmt->execute();
+                $stmt -> bind_result($r_uid);
+                $stmt->fetch();
+                $success = "Success";
+            }
+            else{
+                $success = "Failed";
+            }
+        //}
+        //else{
+            
+        //}
+        $results[] = array('result'=>$success,'comment'=>$comment,'rid'=>$rid,'reply_count'=>reply_count,'comment_count'=>comment_count,'creation_time'=>creation_time);
+        $stmt -> close();
+        $db->close();
+        
+        echo json_encode($results);
+    }
     else {
         $data['error'] = 'The Service you asked for was not recognized';
         echo json_encode($data);
