7766c7805ed9152e78cccf963f3b21d094fec897
aoisky <yangdon8942@gmail.com>
2013-11-21 21:36:42 -0500

    Implement parcelable for UserInfo

--- VoiceMe/src/me/imvoice/app/UserInfo.java
+++ VoiceMe/src/me/imvoice/app/UserInfo.java
@@ -1,14 +1,16 @@
 package me.imvoice.app;
 
 import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * A class to save user information
  * @author Yudong Yang
  *
  */
-public class UserInfo {
-
+public class UserInfo implements Parcelable{
+	private int mData;
 	private int uid;
 	private String userName;
 	private Drawable userAvatar;
@@ -48,5 +50,31 @@ public class UserInfo {
 	public String getmd5Password(){
 		return md5Password;
 	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int arg1) {
+		// TODO Auto-generated method stub
+		out.writeInt(mData);
+	}
 	
+    public static final Parcelable.Creator<UserInfo> CREATOR
+    	= new Parcelable.Creator<UserInfo>() {
+    	public UserInfo createFromParcel(Parcel in) {
+    	return new UserInfo(in);
+	}
+
+	public UserInfo[] newArray(int size) {
+		return new UserInfo[size];
+	}
+    };
+
+	private UserInfo(Parcel in) {
+		mData = in.readInt();
+	}
 }
