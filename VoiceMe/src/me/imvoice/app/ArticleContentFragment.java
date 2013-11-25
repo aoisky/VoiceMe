830b649ef76935a0b15f368a8e83a0edb5ad69ba
Deen Liu <liu.declan@gmail.com>
2013-11-25 01:45:19 -0500

    Updated article content fragment.

--- VoiceMe/src/me/imvoice/app/ArticleContentFragment.java
+++ VoiceMe/src/me/imvoice/app/ArticleContentFragment.java
@@ -1,5 +1,12 @@
 package me.imvoice.app;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,19 +15,24 @@ import me.imvoice.app.ArticleFragment.myArticleAdapter;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -44,14 +56,49 @@ public class ArticleContentFragment extends Fragment {
 	
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
-	    ArticleTitle title = new ArticleTitle("This is an Article Title", new ColorDrawable(Color.parseColor("#00A9FF")), "I am the author");
-	    ArticleContent content = new ArticleContent(new ColorDrawable(Color.parseColor("#0000FF")), "balabala.");
+	    ArticleTitle title = new ArticleTitle("Purdue helps bring SplashCon to Indianapolis; provides unique opportunity to Indiana computer science students", new ColorDrawable(Color.parseColor("#00A9FF")), "I am the author");
+	    ArticleContent content = new ArticleContent("https://www.google.com/images/srpr/logo11w.png", ""
+	    		+"WEST LAFAYETTE, Ind. - Computer scientists from Purdue University led the organization of the 2013 Splash Conference, and helped bring the top software development conference to Indianapolis.\n\n"
+	    		+"\"This is the first time this important conference has been held in our area and is easily accessible to students in Indiana and the Midwest,\" said Hosking, an associate professor of computer science. “There are already more than 30 Purdue undergraduate students signed up to go for whom travel to past locations would have been prohibitively expensive. We really want to encourage students to take advantage of this opportunity to learn from giants in the field and to network with other students and industry representatives.\"\n\n"
+	    		+"Purdue students can attend the conference for free on Tuesday (Oct. 29) and the university is providing bus transportation from the West Lafayette campus to and from the conference for any students within the area. Students interested in riding the bus should contact Pat Morgan at pam@cs.purdue.edu.");
 	    Article article = new Article(title, content);
 	    
-	    articles.add(article);
+	    setView(this.getView(), article);
 	    
-//	    MyPagerAdapter articleAdapter = new MyPagerAdapter();
-//	    pagerView.setAdapter(articleAdapter);
+	}
+	
+	public class RetreiveFeedTask extends AsyncTask<String, Void, Void> {
+
+	    private Exception exception;
+	    private Bitmap bitmap = null;
+
+	    protected Void doInBackground(String... urls) {
+	    	URL url;
+	    	HttpURLConnection connection;
+			try {
+				
+				url = new URL(urls[0]);
+				connection = (HttpURLConnection) url.openConnection();
+				connection.setDoInput(true);
+		        connection.connect();
+		        InputStream input = connection.getInputStream();
+		        bitmap = BitmapFactory.decodeStream(input);
+		        
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+			return null;
+	    }
+
+	    protected void onPostExecute(Void feed) {
+	    	ImageView picView = (ImageView)getView().findViewById(R.id.article_pic);
+	    	picView.setImageBitmap(bitmap);
+	    }
 	}
 	
 	private void setButtonColor(View view){
@@ -63,58 +110,27 @@ public class ArticleContentFragment extends Fragment {
 		share_btn.setBackgroundResource(R.drawable.aritcle_btns);
 	}
 	
-//	public class MyPagerAdapter extends PagerAdapter {
-//
-//		@Override
-//		public Object instantiateItem(View collection, int position) {
-//		    Article article = articles.get(position);
-//
-//		    View view = inflater.inflate(R.layout.article_content_fragment, null);
-//
-//		    setView(view, article);
-//
-//		    ((ViewPager) collection).addView(view);
-//
-//		    return view;
-//		}
-//		
-//		@Override
-//		public void destroyItem(View collection, int position, Object view) {
-//		     ((ViewPager) collection).removeView((View) view);
-//		}
-//		
-//		@Override
-//		public int getCount() {
-//			// TODO Auto-generated method stub
-//			return 0;
-//		}
-//
-//		@Override
-//		public boolean isViewFromObject(View arg0, Object arg1) {
-//			// TODO Auto-generated method stub
-//			return false;
-//		}
-//		
-//		private void setView(View view, Article article) {
-//			String title = article.getTitle().getTitle();
-//			Drawable avatar = article.getTitle().getAvatar();
-//			String author = article.getTitle().getAuthor();
-//			Drawable pic = article.getContent().getPic();
-//			String content = article.getContent().getContent();
-//			
-//			TextView titleView = (TextView)view.findViewById(R.id.article_title);
-//			ImageView avatarView = (ImageView)view.findViewById(R.id.article_avatar);
-//			TextView contentView = (TextView)view.findViewById(R.id.article_content);
-//			ImageView picView = (ImageView)view.findViewById(R.id.article_pic);
-//			
-//			titleView.setText(title);
-//			avatarView.setImageDrawable(avatar);
-//			contentView.setText(content);
-//			picView.setImageDrawable(pic);
-//		}
-//		
-//	}
-//	
+	private void setView(View view, Article article) {
+		String title = article.getTitle().getTitle();
+		Drawable avatar = article.getTitle().getAvatar();
+		String author = article.getTitle().getAuthor();
+		String picUrl = article.getContent().getPicUrl();
+		String content = article.getContent().getContent();
+		
+		TextView titleView = (TextView)view.findViewById(R.id.article_title);
+		ImageView avatarView = (ImageView)view.findViewById(R.id.article_avatar);
+		TextView contentView = (TextView)view.findViewById(R.id.article_content);
+		
+		titleView.setText(title);
+		avatarView.setImageDrawable(avatar);
+		contentView.setText(content);
+		
+		String[] urls = new String[1];
+		urls[0] = picUrl;
+		RetreiveFeedTask task = new RetreiveFeedTask();
+		task.execute(urls);
+	}
+	
 	public class Article{
 		private ArticleTitle title;
 		private ArticleContent content;
@@ -158,16 +174,16 @@ public class ArticleContentFragment extends Fragment {
 	}
 	
 	public class ArticleContent{
-		private Drawable pic;
+		private String picUrl;
 		private String content;
 		
-		public ArticleContent(Drawable pic, String content) {
-			this.pic = pic;
+		public ArticleContent(String picUrl, String content) {
+			this.picUrl = picUrl;
 			this.content = content;
 		}
 		
-		public Drawable getPic() {
-			return pic;
+		public String getPicUrl() {
+			return picUrl;
 		}
 		
 		public String getContent() {
