53823f982be6296090e6830fb25850d4c0ad5d24
aoisky <yangdon8942@gmail.com>
2013-11-28 17:14:30 -0500

    Update signup detail fragment

--- VoiceMe/src/me/imvoice/app/SignupDetailsFragment.java
+++ VoiceMe/src/me/imvoice/app/SignupDetailsFragment.java
@@ -4,15 +4,27 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class SignupDetailsFragment extends Fragment{
-
+public class SignupDetailsFragment extends Fragment implements OnClickListener{
+	
+	private Button registerBtn;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_signup_details,
 				container, false);
+		registerBtn = (Button)rootView.findViewById(R.id.signup_button_register);
+		registerBtn.setOnClickListener(this);
 		return rootView;
+	}
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+		getActivity().finish();
+		
 	}
 }
